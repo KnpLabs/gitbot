3757f863e345f4a1af61a8f2be1646e76349d183
Antoine Lelaisant <antoine.lelaisant@knplabs.com>
2016-10-07 11:58:10 +0200

    Add gitbot start

--- apps/repository-api/lib/controller/GetRepository.js
+++ apps/repository-api/lib/controller/GetRepository.js
@@ -4,10 +4,8 @@ import Store from '../service/Store';
 export default function GetRepository(req, res, next) {
   const name  = req.params.name;
 
-  const repository = new Repository({name});
-
   Store
-    .find(repository)
+    .find(name)
     .then(repository => res.status(200).json(repository))
     .catch(err => next(err))
   ;
--- apps/repository-api/lib/service/HookManager.js
+++ apps/repository-api/lib/service/HookManager.js
@@ -1,8 +1,30 @@
-const github = require('github');
+const github = require('github')();
+const logger = require('tracer').console();
 
 export default class HookManager {
-  static createHook() {
-    github.
+  static createHook(repository) {
+    github.authenticate({
+      type: "token",
+      token: repository.token
+    });
+
+    const [user, repositoryName] = repository.name.split('/');
+    logger.debug(user, repositoryName);
+
+    github.repos.createHook({
+      user: user,
+      repo: repositoryName,
+      name: "web",
+      config: {
+        url: "http://gitbot.antoinelelaisant.ultrahook.com/hook",
+        "content_type": "json"
+      },
+      active: true,
+      events: ["*"]
+    }).then(() => {
+      logger.debug('test');
+    })
+      .catch(err => { logger.debug(err); });
   }
 
   static deleteHook() {
