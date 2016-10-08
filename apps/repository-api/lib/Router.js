a8783409fec4627ff7e5ec629b3d12887477d8c8
Albin Kerouanton <albin.kerouanton@knplabs.com>
2016-10-08 12:43:59 +0200

    Make hook url configurable

--- apps/repository-api/lib/Router.js
+++ apps/repository-api/lib/Router.js
@@ -6,13 +6,16 @@ import GetRepository from './controller/GetRepository';
 import StartBot from './controller/StartBot';
 import StopBot from './controller/StopBot';
 
-export default function Router() {
+export default function Router(manager) {
   const router = express.Router();
+
+  const startBot = new StartBot(manager);
+  const stopBot = new StopBot(manager);
 
   router.get('/repository/:name', GetRepository);
   router.put('/repository/:name', PutRepository);
-  router.post('/repository/:name/start', StartBot);
-  router.post('/repository/:name/stop', StopBot);
+  router.post('/repository/:name/start', startBot.handleRequest.bind(startBot));
+  router.post('/repository/:name/stop', stopBot.handleRequest.bind(stopBot));
 
   router.param('name', (req, res, next, name) => {
     if (!/^[\w\-_]+\/[\w\-_]+$/.test(name)) {
